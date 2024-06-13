db0a349772452b6597af10bee71c362a8cf09ac0
james <james@lappy.pdx.2600hz.com>
2024-06-13 15:43:56 -0700

    OTP-26: Update gen_smtp_server with start_link

--- make/deps.mk
+++ make/deps.mk
@@ -107,7 +107,7 @@ dep_ersip = git https://github.com/2600hz/erlang-ersip 5125e187807eff8dd507baa7f
 # dep_fs_event = git https://github.com/jamhed/fs_event 783400da08c2b55c295dbec81df0d926960c0346
 # dep_fs_sync = git https://github.com/jamhed/fs_sync 2cf85cf5861221128f020c453604d509fd37cd53
 
-dep_gen_smtp = git https://github.com/2600hz/erlang-gen_smtp 7e57b573fe55b69aaff8126c7a662b1a6c00bf9f
+dep_gen_smtp = git https://github.com/2600hz/erlang-gen_smtp aa45101978aabb38af9156e18ba37b9dc2fa3d83
 # used by teletype, notify, and fax
 # branch 2600Hz-otp-26-fixed
 
